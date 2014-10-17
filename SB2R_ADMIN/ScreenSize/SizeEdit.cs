e7f06cfba2dd00510fa0873ef69a7b3d373d24a3
MuteLovestone <mutelovestone@gmail.com>
2014-10-17 15:33:47 -0600

    updating all branches with all misses things

---
+++ SB2R_ADMIN/ScreenSize/SizeEdit.cs
@@ -0,0 +1,40 @@
+﻿// =SB2Revival Engine Upgrade=
+// =C#/XNA convertion from Rpg Maker=
+// =Programmers=
+// =Mute Lovestone=
+// =SizeEdit.cs=
+// = 10/17/2014 =
+// =SB2R_ADMIN=
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using SB2R_MU001.Screens.AllOptions;
+namespace SB2R_ADMIN.ScreenSize
+{
+    public partial class SizeEdit : Form
+    {
+        private ScreenOpt temp = new ScreenOpt();
+        public SizeEdit()
+        {
+            this.InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.temp.AddOp(Convert.ToInt32(this.numericUpDown1.Value), Convert.ToInt32(this.numericUpDown2.Value), this.checkBox1.Checked);
+            this.set(Convert.ToInt32(this.numericUpDown1.Value), Convert.ToInt32(this.numericUpDown2.Value), this.checkBox1.Checked);
+        }
+        private void set(int x1, int x2, bool x3)
+        {
+            string temp = string.Format("{0}X{1} FullScreen: {2}", x1, x2, x3);
+            this.listBox1.Items.Add(temp);
+        }
+        private void write()
+        {
+            this.temp.write();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.write();
+        }
+    }
+}
---
+++ SB2R_MU001/Screens/Active/AcScreen.cs
@@ -0,0 +1,56 @@
+﻿// =SB2Revival Engine Upgrade=
+// =C#/XNA convertion from Rpg Maker=
+// =Programmers=
+// =Mute Lovestone=
+// =AcScreen.cs=
+// = 10/17/2014 =
+// =SB2R_MU001=
+using System;
+using System.IO;
+using System.Linq;
+namespace SB2R_MU001.Screens.Active
+{
+    /// <summary>
+    /// Used so that it can automaticly change to your perfect screen size
+    /// <para>Enjoy</para>
+    /// </summary>
+    public class AcScreen
+    {
+        #region Size
+        public Int32 Height = 768;
+        public Int32 Width = 1024;
+        public bool Full = false;
+        #endregion
+        #region Reader
+        public void reader()
+        {
+            using (BinaryReader temp = new BinaryReader(File.Open(string.Format("{0}{1}", Environment.CurrentDirectory, @"/ActiveScreen.Mu1"), FileMode.Open)))
+            {
+                this.Height = temp.ReadInt32();
+                this.Width = temp.ReadInt32();
+                this.Full = temp.ReadBoolean();
+            }
+        }
+        public void Writer()
+        {
+            using (BinaryWriter temp = new BinaryWriter(File.Open(string.Format("{0}{1}", Environment.CurrentDirectory, @"/ActiveScreen.Mu1"), FileMode.Create)))
+            {
+                temp.Write(this.Height);
+                temp.Write(this.Width);
+                temp.Write(this.Full);
+            }
+        }
+        public void Checker()
+        {
+            try
+            {
+                this.reader();
+            }
+            catch
+            {
+                this.Writer();
+            }
+        }
+        #endregion
+    }
+}
